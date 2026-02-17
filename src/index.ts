d591477e0a4b53990e348a9ac2368d87c5cfab89
Gabriel Sillam <ggthesisi@protonmail.com>
2026-02-17 11:03:28 +0200

    Added validation logic

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { type Context, Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { cors } from 'hono/cors'
 import { webhookUpdateSchema } from './Models/webhookUpdateSchema.js'
 import { zValidator } from '@hono/zod-validator'
-import { z } from 'zod'
-import {webhookTestSchema} from "./Models/webhookTestSchema.js";
+import { z, type ZodError } from 'zod'
+import { webhookTestSchema } from "./Models/webhookTestSchema.js";
 
-const unionType = z.union([webhookUpdateSchema , webhookTestSchema])
+const unionType = z.union([webhookUpdateSchema, webhookTestSchema])
 
 const webhook = new Hono()
-    .use(logger())
-    .use(cors())
-    .use(zValidator(
-        'json',unionType
-        )
-    )
-    .basePath('/webhook')
+  .use(logger())
+  .use(cors())
+  .use(zValidator(
+    'json', unionType
+  )
+  )
+  .basePath('/webhook')
 
-
+webhook.post(
+  '/',
+  async (c: Context) => {
+    const body = await c.req.json()
+    // check if the body is of type webhookUpdateSchema or webhookTestSchema
+    if (webhookUpdateSchema.safeParse(body).success) {
+      console.log('webhookUpdateSchema')
+    } else if (webhookTestSchema.safeParse(body).success) {
+      console.log('webhookTestSchema')
+    } else {
+      return c.json({ error: 'Invalid body' }, 400)
+    }
+    return c.json({ message: 'Webhook received' }, 200)
+  }
+)
 
 serve({
   fetch: webhook.fetch,
