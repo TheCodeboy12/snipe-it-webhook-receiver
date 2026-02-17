1340ab2508e4ea71e1b5d50ae80a338684bd23ce
Gabriel Sillam <ggthesisi@protonmail.com>
2026-02-17 10:35:54 +0200

    created webhook path

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { cors } from 'hono/cors'
+import { webhookUpdateSchema } from './Models/webhookUpdateSchema.js'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+import {webhookTestSchema} from "./Models/webhookTestSchema.js";
 
-const app = new Hono()
+const unionType = z.union([webhookUpdateSchema , webhookTestSchema])
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const webhook = new Hono()
+    .use(logger())
+    .use(cors())
+    .use(zValidator(
+        'json',unionType
+        )
+    )
+    .basePath('/webhook')
+
+
 
 serve({
-  fetch: app.fetch,
+  fetch: webhook.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
